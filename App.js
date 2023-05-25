672c4d0bf8ef6ebc352ce35dbf7d67ad35ed5a9a
KeplerJFPC <L19211160@tijuana.tecnm.mx>
2023-05-24 17:22:24 -0700

    kalalsdas

--- App.js
+++ App.js
@@ -8,61 +8,41 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //pantallas
-import PersonajesScreen from './Screens/Personaje';
+import PersonajesScreen from './Screens/PersonajesScreen';
 import DetailsScreen from './Screens/DetailsScreen';
 import HomeScreen from './Screens/HomeScreen';
 
 const Tab = createBottomTabNavigator();
-const personajesStack = createStackNavigator();
+const PersonajeStack = createStackNavigator();
 
-function PersonajesStack() {
+function PersonajeStacksScreen() {
   return (
-    <personajesStack.Navigator>
-      <personajesStack.Screen name="Personaje" component={PersonajesScreen}/>
-      <personajesStack.Screen name="DetailsScreen" component={DetailsScreen}
+    <PersonajeStack.Navigator>
+      <PersonajeStack.Screen name="Personaje" component={PersonajesScreen}/>
+      <PersonajeStack.Screen name="DetailsScreen" component={DetailsScreen}
         options={({ route }) => ({ headerShown: true, title: route.params.user.name })}
       />
-    </personajesStack.Navigator>
+    </PersonajeStack.Navigator>
   );
 }
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{ //Versiones anteriores tabBarOptions
-          tabBarActiveTintColor: '#ff6600',//activeTintColor
-          tabBarInactiveTintColor: "#060606",//inactiveTintColor
-          tabBarShowLabel: true,//showLabel
-          tabBarLabelStyle: { //labelStyle
-            fontSize: 12
-          },
-          tabBarStyle: {//style
-            paddingBottom: 5,
-            backgroundColor: "#f3f3f1"
-          }
-        }}>
-          <Tab.Screen name="Home" component={HomeScreen} options={{
-            tabBarLabel: "Inicio",
-            tabBarIcon: ({ color }) => (
-              <Ionicons name={"ios-home"} size={20} color={color} />
-            )
-          }} />
-          <Tab.Screen name="Personajes" component={PersonajesStack} options={{
-            tabBarLabel: "Personajes",
-            tabBarIcon: ({ color }) => (
-              <Ionicons name={"ios-people"} size={20} color={color} />
-            )
-          }} />
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} options={{
+          tabBarLabel: "Inicio",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name={"ios-home"} size={20} color={color} />
+          )
+        }} />
+        <Tab.Screen name="Personajes" component={PersonajeStacksScreen} options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name={"ios-people"} size={20} color={color} />
+          )
+        }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
 
